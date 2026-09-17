0dd771c842399487dd0c53bd517b0b89b65cef07
Toma Barbarić <tomabarbaric1@gmail.com>
2026-09-17 06:50:10 -0700

    Update randomshit.s

--- randomshit.s
+++ randomshit.s
@@ -3,13 +3,12 @@
 
 .text
 _start:
-xor rcx,rcx
-xor rax,rax
-call _strlen
-call _print
+
+
+lea rax,[s1] # arg
+call _print  # print(arg)
 
 _strlen:
- lea rax,[s2]
  str_loop:
  mov bl,[rax]
  cmp bl,0
@@ -22,17 +21,20 @@ _strlen:
  ret
 
 _print:
- ;#int3
+ mov r8,rax
+ call _strlen
+ int3
  mov r9,rax
  xor rax,rax
 
+ mov rsi,r8
  mov rax,1
  mov rdi,1
- lea rsi,[s2]
  mov rdx,r9
+
  syscall
 
 .data
 s1: .string "123456789"
-s2: .string "string2"
+s2: .string "masovanstring768"
 .bss
